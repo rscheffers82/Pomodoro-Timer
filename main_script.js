0de2b239c6d1953dc0df45a29429ee1ef92c3deb
Roy Scheffers <r.scheffers@gmail.com>
2016-07-11 19:49:05 -0400

    Add final styling and sound

--- main_script.js
+++ main_script.js
@@ -18,8 +18,13 @@ var sandTimer = (function(){
 	var loop;				// timer, used for tracking time, work and break time
 
 	var degrees = 0;		// at what angle is the sand timer
-	
+	var bellSound = new Audio('audio/small-bell.wav');
+
 	// private functions
+	var error = function(message){
+		console.log(message);
+	}
+
 	var disableSliders = function(disable){
 	 	if( disable ){
 	 		$('.choice').attr('disabled', true);
@@ -74,14 +79,13 @@ var sandTimer = (function(){
 	}
 
 	var render = function(fill, rotate){
-		console.log('render - fill: ',fill);
+		//console.log('render - fill: ',fill);
 
 		ctx.save();
 		clearCanvas();
 
-		fill *= 1.5;								// 100% = 150 pixels on the canvas
+		fill *= 1.5;								// 100% = 150 pixels container size on the canvas
 
-		// line thickness, stroke, fill
 		drawTop(10, '#222223');						// outer layer
 		drawTop(7, '#fafafc', '#fafafc');			// inner white layer, sand separator
 		ctx.save();									// save state so we can go back to it after clipping
@@ -102,51 +106,15 @@ var sandTimer = (function(){
 		ctx.fillRect(0, 350-fill, 400, 0+fill);	// 100% full (start 200, end 350), 0
 		ctx.restore();
 
+		// draw falling sand
 		ctx.save();
 		fill /= 1.5;
-		if ( fill > 1 && fill < 100 ) {
+		if ( fill > 0 && fill < 100 ) {
 			ctx.fillStyle = '#d2d233';
 			ctx.fillRect(193, 195, 14, 155);
 		}
 		ctx.restore();
-		// draw falling sand
-		/*	
-		fill /= 1.5;
-		ctx.fillStyle = '#d2d233';
-		if ( !rotate && fill === 1 ){
-			console.log('fill 0');
-			var level = 0;
-			var fall = window.setInterval(fallingSand, 25);
-			function fallingSand(){
-				//ctx.fillStyle = '#d2d233';
-				ctx.fillRect(193, 195, 14, level);
-				level += 5;
-				if ( level > 155 ) window.clearInterval(fall);
-			}
-		} else if ( !rotate && fill >= 100 ){
-			window.clearInterval(loop);			// stop the timer
-			//ctx.fillStyle = '#d2d233';
-			ctx.fillRect(193, 195, 14, 10);
-			var fall = window.setInterval(fallingSand, 200);
-			var level = 0;
-			function fallingSand(){
-				console.log('falling sand, level', level);
-				//ctx.fillStyle = '#fafafc';
-				ctx.fillRect(193, 195, 14, level);
-				level++;
-				if ( level > 7 ) {
-					window.clearInterval(fall);
 
-				}
-			}
-			//	ctx.fillRect(193, 195, 14, 155);
-			console.log('fill 100');
-			//}
-		} else if ( !rotate && fill > 1 && fill < 100 ) {
-			ctx.fillRect(193, 195, 14, 155);
-			console.log('fill ',fill);
-		}
-*/
 		ctx.restore();
 	 }
 
@@ -175,8 +143,6 @@ var sandTimer = (function(){
 	}
 
 	var switchStartBreak = function() {
-		console.log('switchStartBreak - workBreak: ', workBreak);
-		// rotate timer
 		rotateTimer();
 		
 		// reset core vars and alternate between work / break
@@ -189,13 +155,14 @@ var sandTimer = (function(){
 	function mainLoop(){		
 		// still time to go until the end?
 		if ( secondsPassed < cycleEnd ){
-			secondsPassed += 0.250 * 100;								// 250 milisecond interval
-			var sandLevel = Math.round( ( secondsPassed / cycleEnd ) * 100 );
+			secondsPassed += 0.250;								// 250 milisecond interval
+			var sandLevel = ( secondsPassed / cycleEnd ) * 100;
 			render( sandLevel );
 		} else {
-			// end of the cycle, turn timer and start with the break
+			// end of the cycle, turn timer and start with the break or new work round
 			timerRunning = false;
 			window.clearInterval(loop)
+			bellSound.play();
 			switchStartBreak();
 			window.setTimeout(function(){ 
 				sandTimer.startStop(); 
@@ -209,7 +176,7 @@ var sandTimer = (function(){
 	return{
 	init: function(){
 		c = document.querySelector('#c');
-		//if ( !c.getContext ) error('no canvas'); return	// break script when canvas s not supported or can be found
+		//if ( !c.getContext ) error('your device / browser does not support a canvas'); break	// break script when canvas s not supported or can be found
 
 		// assign canvas context, and scale appropriately
 		ctx = c.getContext('2d');
@@ -223,12 +190,7 @@ var sandTimer = (function(){
 		cycleEnd = workTime * 60; 	 	// assign cycle the work value
 	 	render(0, false);
 	 },
-	error: function(message){
-		//console.log(message);
-		// display error on screen 
-	},
 	startStop: function(){
-
 		if (!timerRunning) {			// start or continue
 			timerRunning = true;
 			disableSliders(true);
@@ -272,84 +234,5 @@ $('#bt').on('input', function(){
 	$('.breaktime').text( sandTimer.updateBreakTime(this.value) );
 });
 
-// CanvasRenderingContext2D.isPointInPath() could be used for an onclick event to stop the timer
-// fill animation: http://stackoverflow.com/questions/23460873/how-can-i-fill-canvas-shape-with-animation
+// fill animation example: http://stackoverflow.com/questions/23460873/how-can-i-fill-canvas-shape-with-animation
 // jsfiddle: http://jsfiddle.net/jM4hW/18/
-
-/* === Commented Javascript for understanding ===
-
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var cw = canvas.width;
-var ch = canvas.height;
-ctx.lineCap = "round";
-
-var y = ch - 10;
-var drawingColor = "#0092f9";
-animate();
-
-function animate() {
-
-    if (y > 0) {
-        requestAnimationFrame(animate);
-    }
-
-    ctx.clearRect(0, 0, cw, ch);
-    ctx.save();
-    //drawContainer(0, null, null);
-    drawContainer(15, drawingColor, null); // outer blue line
-    drawContainer(7, "white", "white"); // inner white spacing
-    ctx.save();
-    ctx.clip();		// ensure that the animation only happens within the shape
-    drawLiquid();
-    ctx.restore();
-    y--; // decreate y so the line moves up
-
-}
-
-function drawLiquid() {
-    ctx.beginPath();
-    ctx.moveTo(0, y);
-    for (var x = 0; x < cw; x +=30) {
-       ctx.quadraticCurveTo(x + 10, y + 20, x + 30, y); // create the waves top of the water level, top left to top right
-    }	
-    ctx.lineTo(cw, ch);			// draw a line from the waves to the bottom right
-    ctx.lineTo(0, ch);			// draw a line from the bottom right to left bottom corner
-    ctx.closePath();				// now we have our square close it
-    ctx.fillStyle = drawingColor;		// set color
-    ctx.fill();											// fill the rectangle
-}
-
-function drawContainer(linewidth, strokestyle, fillstyle) {
-    ctx.beginPath();
-    ctx.moveTo(109, 15);
-    ctx.bezierCurveTo(121, 36, 133, 57, 144, 78);
-    ctx.bezierCurveTo(160, 109, 176, 141, 188, 175);
-    ctx.bezierCurveTo(206, 226, 174, 272, 133, 284);
-    ctx.bezierCurveTo(79, 300, 24, 259, 25, 202);
-    ctx.bezierCurveTo(25, 188, 30, 174, 35, 161);
-    ctx.bezierCurveTo(53, 115, 76, 73, 100, 31);
-    ctx.bezierCurveTo(103, 26, 106, 21, 109, 15);
-    ctx.lineWidth = linewidth;
-    ctx.strokeStyle = strokestyle;
-    ctx.stroke();
-    if (fillstyle) {
-        ctx.fillStyle = fillstyle;
-        ctx.fill();
-    }
-}
-
-*/
-
-/*
-	//ctx.fill();
-	ctx.setTransform(1, 0, 0, 1, 0, 0); // reset the position back to 0,0
-*/
-
-
-	/* adding shadow
-  	ctx.shadowOffsetX = 5;
-  	ctx.shadowOffsetY = 5;
-  	ctx.shadowBlur = 10;
-  	ctx.shadowColor = "DarkGoldenRod";
-	*/
